0868a1e76cb0f5900b681a72da1bc8d5fb301dbe
Katy Pedelty <katy.ryan.pedelty@gmail.com>
2018-02-14 14:17:03 -0800

    Did way more things than I should have in 1 commit
    I guess I deleted old content from the Rails app that I copied. And I
    sort of twiddled with the first page and design, plus started on getting
    a page to show a single blog at a time.
    
    It would be nice if I would do some of the transitions in javascript
    rather than as page loads. But I'm not gonna do that till I know if this
    is actually even going to be anything.

--- app/controllers/content_controller.rb
+++
@@ -1,6 +0,0 @@
-class ContentController < ApplicationController
-  def index
-    @asset_path = "comics/xkcd/november_2016_2x.png"
-    @good_url = "http://google.com"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'content#index'
-  get 'content/index'
-  get 'welcome/index'
+  root to: 'pages#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/index'
+
+  resources :posts, only: [:show]
 end
