59af1ffe58f6a14bd60f5067c1b6f728e426d03c
Katy Pedelty <katy.ryan.pedelty@gmail.com>
2018-02-15 13:48:47 -0800

    Add fonts and icons
    I don't think I like any of these fonts, actually, so expect them to
    change again soon.
    
    I keep forgetting to make atomic commits. I guess Google broke my git
    training.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.first
+    # Current post
+    @id = params['id'].to_i
+    @post = Post.find(@id)
     @post.htmlize!
+
+    # Previous post
+    if @id != 1
+      @previous_post = post_path(@id - 1)
+    end
+
+    # Next post
+    if @id != Post.count
+      @next_post = post_path(@id + 1)
+    end
   end
 end
