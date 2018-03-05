4120b429c5fcb149947b6a4f07c8098f20ccbf71
Katy Pedelty <katy.ryan.pedelty@gmail.com>
2018-03-05 13:35:48 -0800

    Add a display date for posts
    I don't have model tests... or any tests, for that matter. I don't know
    if anyone is even going to see this blog, though. I'm sure I'll add some
    tests if I keep it up.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 require 'redcarpet'
 
 class Post < ApplicationRecord
-  # attr_accessor :content
+  before_validation :set_posted_date
+
+  def set_posted_date
+    if !posted_date
+      self.posted_date = Time.now
+    end
+  end
 
   def htmlize!
     renderer = Redcarpet::Render::HTML.new
